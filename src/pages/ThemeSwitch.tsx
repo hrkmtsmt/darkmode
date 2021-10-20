b20017a36cb735150d57c8b69c928185480d51e8
hrkmtsmt <hrkmtsmt@icloud.com>
2021-10-20 23:39:49 +0900

    :+1: feature: ToggleSwitch.tsxを作成

---
+++ src/pages/ThemeSwitch.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const Track = styled.button`
+  display: flex;
+  justify-content: flex-start;
+  color: var(--primary-color);
+  background: var(--primary-color);
+  width: 80px;
+  height: 40px;
+  padding: 4px;
+  border-radius: 40px;
+  transition: ease-in-out 0.2s;
+  z-index: 999;
+`;
+
+const Thumb = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  border-radius: 32px;
+  width: 32px;
+  height: 32px;
+  background: var(--base-color);
+  transition: ease-in-out 0.2s;
+  animation: a-sun-moon 1.6s linear infinite alternate;
+  position: relative;
+  overflow: hidden;
+`;
+
+const ThumbInner = styled.div`
+  font-size: 24px;
+  display: flex;
+  position: absolute;
+  top: -32px;
+  transform: rotateY(0);
+  transition: ease-in-out 0.4s;
+  &:last-child {
+    top: 0px;
+  }
+  &::before {
+    width: 32px;
+    height: 32px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+export const ThemeSwitch = () => {
+  return (
+    <Track>
+      <Thumb>
+        <ThumbInner></ThumbInner>
+      </Thumb>
+    </Track>
+  );
+};
