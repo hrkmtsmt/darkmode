006ef5f99eded1e734e3121f3808abae3f1bcf8a
hrkmtsmt <hrkmtsmt@icloud.com>
2021-11-06 21:05:13 +0900

    :pencil2: edit file: _app.tsxを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import type { AppProps } from "next/app";
-import { ResetCSS } from "@src/styles/ReseCSS";
 import { GlobalStyle } from "@src/styles/GlobalStyle";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <React.Fragment>
-      <ResetCSS />
       <GlobalStyle />
       <Component {...pageProps} />
     </React.Fragment>
